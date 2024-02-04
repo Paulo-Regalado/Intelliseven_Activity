d247f18697da073be0f7dcc3fc7b400827cb6009
Regalado-Paulo <paulo.regalado.s@bulsu.edu.ph>
2024-02-04 11:30:48 +0800

    See more updated

--- javascript/home_chat.js
+++ javascript/home_chat.js
@@ -5,4 +5,32 @@ document.addEventListener("DOMContentLoaded", function () {
     showMoreButton.addEventListener("click", function () {
         seeMoreDiv.classList.toggle("d-none");
     });
+
+    var customedBtn = document.getElementById("customed_chat");
+    var customedDiv = document.getElementById("customed_chat_option");
+
+    customedBtn.addEventListener("click", function () {
+        customedDiv.classList.toggle("d-none");
+    });
+
+    var chat_membersBtn = document.getElementById("chat_members");
+    var chat_membersDiv = document.getElementById("chat_members_option");
+
+    chat_membersBtn.addEventListener("click", function () {
+        chat_membersDiv.classList.toggle("d-none");
+    });
+
+    var mediaBtn = document.getElementById("media");
+    var mediaDiv = document.getElementById("media_option");
+
+    mediaBtn.addEventListener("click", function () {
+        mediaDiv.classList.toggle("d-none");
+    });
+
+    var privacyBtn = document.getElementById("privacy");
+    var privacyDiv = document.getElementById("privacy_option");
+
+    privacyBtn.addEventListener("click", function () {
+        privacyDiv.classList.toggle("d-none");
+    });
 });
